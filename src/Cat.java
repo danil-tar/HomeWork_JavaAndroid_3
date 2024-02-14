c434613b1a558c0e3a30f61caeffa87ca42749d7
Dan <danilviktar@gmail.com>
2024-02-14 14:34:45 +0200

    HomeWork_3 Alternative

--- src/Cat.java
+++ src/Cat.java
@@ -6,4 +6,20 @@ public class Cat {
         this.name = name;
         this.color = color;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,42 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
+        ArrayList<String> wordsList = new ArrayList<>();
+
         boolean isEnd = false;
         Scanner scanner = new Scanner(System.in);
         while (!isEnd) {
-            System.out.print("Enter a word: ");
+            System.out.print("Enter a next word: ");
             String word = scanner.nextLine();
             if (!word.equals("end")) {
-                System.out.println("You entered: "+ word);
+                wordsList.add(word);
             } else {
                 isEnd = true;
+                System.out.println("Your words: ");
+                wordsList.forEach(System.out::println);
                 System.out.println("Goodbye! \n");
             }
         }
 
+
+        Cat[] catsArray = new Cat[10];
+
+        catsArray[0] = new Cat("Барсик", "рыжий");
+        catsArray[1] = new Cat("Мурзик", "черный");
+        catsArray[2] = new Cat("Рыжик", "рыжий");
+        catsArray[3] = new Cat("Мурка", "черная");
+        catsArray[4] = new Cat("Васька", "белый");
+        catsArray[5] = new Cat("Томасина", "серый");
+        catsArray[6] = new Cat("Кристина", "рыжая");
+        catsArray[7] = new Cat("Пушок", "белый");
+        catsArray[8] = new Cat("Дымка", "серый");
+        catsArray[9] = new Cat("Кузя", "черный");
+
+
         Map<String, String> cats = new HashMap<>();
-        cats.put("Барсик", "рыжий");
-        cats.put("Мурзик", "черный");
-        cats.put("Рыжик", "рыжий");
-        cats.put("Мурка", "черная");
-        cats.put("Васька", "белый");
-        cats.put("Томасина", "серый");
-        cats.put("Кристина", "рыжая");
-        cats.put("Пушок", "белый");
-        cats.put("Дымка", "серый");
-        cats.put("Кузя", "черный");
+        Arrays.stream(catsArray).forEach(cat -> cats.put(cat.getName(), cat.getColor()));
 
         Set<String> keySet = cats.keySet();
         for (String key : keySet) {
